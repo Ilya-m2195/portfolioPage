e9d16b35c8a05f4d855f57dd4780953492df086e
Ilya_Myasnichenko <129312680+Ilya-m2195@users.noreply.github.com>
2023-09-14 21:33:40 +0600

    updeted projects list

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,14 +2,13 @@ import { isDisabled } from '@testing-library/user-event/dist/utils';
 import './style.css';
 export const Project = ({link, img, title, skills, done}) => {
   return (
-    <li className="project">
-      <a target='_blank' rel="noreferrer" href={link ? link : isDisabled} >
-        <img src={img} alt={title} className="project__img" />
-        <h3 className="project__title">{title}</h3>
-        <p className="project__description">Technologies - {skills}</p>
-        <p className="project__description">{done}</p>
+    <li className={done === 'completed' ? 'project' : 'project project__opacity'}>
+      <a target='_blank' rel='noreferrer' href={link ? link : isDisabled} >
+        <img src={img} alt={title} className='project__img' />
+        <h3 className='project__title'>{title}</h3>
+        <p className='project__description'>Technologies - {skills}</p>
+        <p className='project__description'>status: {done}</p>
       </a>
     </li>
   );
 }
-
--- src/components/project/projectsList.js
+++ src/components/project/projectsList.js
@@ -2,7 +2,7 @@ import img1 from '../../img/projects/image_2023-08-31_14-29-27.png';
 import img2 from '../../img/projects/image_2023-08-31_14-30-22.png';
 import img3 from '../../img/projects/image_2023-08-31_14-32-18.png';
 import img4 from '../../img/projects/photo_2023-08-31_20-58-52.png';
-
+import img5 from '../../img/projects/image_2023-09-14_21-11-41.png';
 
 export const projects = [
   {
@@ -31,6 +31,13 @@ export const projects = [
     skills: 'React, react-hooks, HTML, CSS, JS',
     img: img4,
     link: 'http://my-portfolio.cv71729.tw1.ru/',
+    done: 'completed'
+  },
+  {
+    title: 'Online-store',
+    skills: 'React, react-hooks, HTML, CSS, JS',
+    img: img5,
+    link: 'http://online-store.cv71729.tw1.ru/',
     done: 'completed'
   },
 ]
